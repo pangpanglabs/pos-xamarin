7534d497471503c0f41f8a89cc048985619f0f7d
song.shuaihui <song.shuaihui@eland.co.kr>
2017-07-04 16:39:21 +0800

    修改IndexPage，添加销售，退货页面，将viewmodel中的示例代码放到Sample文件夹中

--- Pos/Views/IndexPageMaster.xaml.cs
+++ Pos/Views/IndexPageMaster.xaml.cs
@@ -20,37 +20,7 @@ namespace Pos.Views
         public IndexPageMaster()
         {
             InitializeComponent();
-
-            BindingContext = new IndexPageMasterViewModel();
             ListView = MenuItemsListView;
-        }
-
-        class IndexPageMasterViewModel : INotifyPropertyChanged
-        {
-            public ObservableCollection<IndexPageMenuItem> MenuItems { get; set; }
-            
-            public IndexPageMasterViewModel()
-            {
-                MenuItems = new ObservableCollection<IndexPageMenuItem>(new[]
-                {
-                    new IndexPageMenuItem { Id = 0, Title = "Page 1" },
-                    new IndexPageMenuItem { Id = 1, Title = "Page 2" },
-                    new IndexPageMenuItem { Id = 2, Title = "Page 3" },
-                    new IndexPageMenuItem { Id = 3, Title = "Page 4" },
-                    new IndexPageMenuItem { Id = 4, Title = "Page 5" },
-                });
-            }
-            
-            #region INotifyPropertyChanged Implementation
-            public event PropertyChangedEventHandler PropertyChanged;
-            void OnPropertyChanged([CallerMemberName] string propertyName = "")
-            {
-                if (PropertyChanged == null)
-                    return;
-
-                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-            #endregion
         }
     }
 }
