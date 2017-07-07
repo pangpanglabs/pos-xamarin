6ef90101a8dd5864e49cda9c4123ab85f43d1040
HOME\Li.ZhenGuo <li.zhenguo@eland.co.kr>
2017-07-07 17:02:34 +0800

    Commit Cart and CatalogVieModel

--- Pos/ViewModels/Sale/CartViewModel.cs
+++ Pos/ViewModels/Sale/CartViewModel.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using Pos.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Pos.ViewModels.Sale
 {
-    public class CartViewModel
+    public class CartViewModel : BaseViewModel
     {
+        public CartViewModel()
+        {
+            MessagingCenter.Subscribe<Cart>(this, "SendCart", (c) => {
+                CurrentCart = c;
+            });
+            MessagingCenter.Subscribe<CartItem>(this, "DeletecartItem", async (i) => {
+                ApiResult<Cart> cartResult = await PosSDK.CallAPI<Cart>("/cart/remove-item", new
+                {
+                    cartId = CartId,
+                    skuId = i.Sku.Id,
+                    quantity = i.Quantity
+                });
+                if (cartResult.Success == true)
+                {
+                    CurrentCart = cartResult.Result;
+                }
+            });
+        }
+        Cart currentCart;
+        public Cart CurrentCart
+        {
+            get { return currentCart; }
+            set { currentCart = value; OnPropertyChanged(); }
+        }
+        public ObservableRangeCollection<CartItem> CartItems
+        {
+            get
+            {
+                return new ObservableRangeCollection<CartItem>(CurrentCart.Items);
+            }
+        }
+        CartItem selectedItem;
+        public CartItem SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; OnPropertyChanged(); }
+        }
+        public string CartId
+        {
+            get
+            {
+                return CurrentCart.Id;
+            }
+        }
     }
 }
