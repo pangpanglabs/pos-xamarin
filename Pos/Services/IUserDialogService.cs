afad3cfcb37b12d7ee9e54096c6bc544a4fdcdf9
HOME\Li.ZhenGuo <li.zhenguo@eland.co.kr>
2017-07-11 10:10:51 +0800

    添加 Ace.UerDialogs , 添加 Rg.Plugins.Popup 添加 UserDialog IF / Instance

---
+++ Pos/Services/IUserDialogService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Services
+{
+    public interface IUserDialogService
+    {
+
+        /// <summary>
+        /// show loading
+        /// userdialog
+        /// https://github.com/aritchie/userdialogs/blob/master/src/Samples/Samples/MainPage.cs
+        /// </summary>
+        void ShowLoading(string msg);
+
+        /// <summary>
+        /// hide loading
+        /// userdialog
+        /// https://github.com/aritchie/userdialogs/blob/master/src/Samples/Samples/MainPage.cs
+        /// </summary>
+        void HideLoading();
+
+        /// <summary>
+        /// alert
+        /// https://github.com/aritchie/userdialogs/blob/master/src/Samples/Samples/MainPage.cs
+        /// </summary>
+        void Alert(string msg, string title);
+
+        /// <summary>
+        /// alert
+        /// https://github.com/aritchie/userdialogs/blob/master/src/Samples/Samples/MainPage.cs
+        /// </summary>
+        /// <param name="msg"></param>
+        void AlertLongText(string msg);
+
+        /// <summary>
+        /// alert
+        /// https://github.com/aritchie/userdialogs/blob/master/src/Samples/Samples/MainPage.cs
+        /// </summary>
+        /// <param name="msg"></param>
+        void ShowToast(string msg);
+
+        /// <summary>
+        /// 输入文本框
+        /// </summary>
+        /// <returns></returns>
+        Task<string> ShowPromt(string title);
+        /// <summary>
+        /// 确认框
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        Task<bool> Confirm(string msg);
+    }
+}
